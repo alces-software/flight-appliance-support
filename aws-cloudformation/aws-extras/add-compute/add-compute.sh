3f2d31cc29ed1191e88fa3f00c103111ce6996a1
Vaughan Jones <vaughan.jones@alces-software.com>
2016-02-05 17:59:17 +0000

    Use built-in ClusterWare `awscli`

--- aws-cloudformation/aws-extras/add-compute/add-compute.sh
+++ aws-cloudformation/aws-extras/add-compute/add-compute.sh
@@ -24,9 +24,10 @@ echo "Number of compute nodes to deploy"
 read COMPUTENUM
 CLUSTERNAME=`cat /opt/clusterware/etc/config.yml | grep name | awk 'NR==1{print $2}'`
 #With AWS credentials - get ASG info
-STACKID=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names | grep AutoScalingGroupName | grep $CLUSTERNAME-ComputeGroup | awk '{print $4}')
-CURRENTCAP=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $STACKID | grep DesiredCapacity | awk '{print $4}')
+AWSDIR="/opt/clusterware/opt/aws/bin/aws"
+STACKID=$($AWSDIR autoscaling describe-auto-scaling-groups --auto-scaling-group-names | grep AutoScalingGroupName | grep $CLUSTERNAME-ComputeGroup | awk '{print $4}')
+CURRENTCAP=$($AWSDIR autoscaling describe-auto-scaling-groups --auto-scaling-group-names $STACKID | grep DesiredCapacity | awk '{print $4}')
 DESIREDCAP=$(($CURRENTCAP+$COMPUTENUM))
 #Add desired number of instances to existing group
-aws autoscaling update-auto-scaling-group --auto-scaling-group-name $STACKID --max-size $DESIREDCAP
-aws autoscaling set-desired-capacity --auto-scaling-group-name $STACKID --desired-capacity $DESIREDCAP
+$AWSDIR autoscaling update-auto-scaling-group --auto-scaling-group-name $STACKID --max-size $DESIREDCAP
+$AWSDIR autoscaling set-desired-capacity --auto-scaling-group-name $STACKID --desired-capacity $DESIREDCAP
