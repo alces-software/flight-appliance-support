dd8fb17f4832ff3edded73b548fc86da28d8fb4c
Vaughan Jones <vaughan.jones@alces-software.com>
2016-02-05 17:23:50 +0000

    Remove now unused bits

--- aws-cloudformation/aws-extras/add-compute/add-compute.sh
+++ aws-cloudformation/aws-extras/add-compute/add-compute.sh
@@ -1,14 +1,5 @@
 #!/bin/bash
-#Fetch required AWS info to launch a new instance
-MAC=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/)
-VPCID=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/vpc-id)
-SUBNETID=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/subnet-id)
-SECGID=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$MAC/security-group-ids)
-AMIID=$(curl -s http://169.254.169.254/latest/meta-data/ami-id)
-INSTANCETYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
 AVAILZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
-CLUSTERMASTER=$(ifconfig eth0 | grep inet | awk 'NR==1{print $2}')
-STACKID=
 #Get AWS keys
 if ! [ -f $HOME/.aws/credentials ]
 then
@@ -31,9 +22,6 @@ EOF
 fi
 echo "Number of compute nodes to deploy"
 read COMPUTENUM
-#Fetch ClusterWare info
-UUID=$(cat /opt/clusterware/etc/config.yml | grep uuid | awk '{print $2}' | tr -d "'")
-TOKEN=$(cat /opt/clusterware/etc/config.yml | grep token | awk '{print $2}' | tr -d "'")
 CLUSTERNAME=`cat /opt/clusterware/etc/config.yml | grep name | awk 'NR==1{print $2}'`
 #With AWS credentials - get ASG info
 STACKID=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names | grep AutoScalingGroupName | grep $CLUSTERNAME-ComputeGroup | awk '{print $4}')
