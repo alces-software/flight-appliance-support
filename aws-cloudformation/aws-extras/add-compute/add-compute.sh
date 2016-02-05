85d6396f2dda80eb1ab9eecf8aa0d8ac00bb29f4
Vaughan Jones <vaughan.jones@alces-software.com>
2016-02-05 17:20:14 +0000

    Instead of use CF template, add nodes to existing ASG

--- aws-cloudformation/aws-extras/add-compute/add-compute.sh
+++ aws-cloudformation/aws-extras/add-compute/add-compute.sh
@@ -8,6 +8,7 @@ AMIID=$(curl -s http://169.254.169.254/latest/meta-data/ami-id)
 INSTANCETYPE=$(curl -s http://169.254.169.254/latest/meta-data/instance-type)
 AVAILZONE=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone | sed 's/.$//')
 CLUSTERMASTER=$(ifconfig eth0 | grep inet | awk 'NR==1{print $2}')
+STACKID=
 #Get AWS keys
 if ! [ -f $HOME/.aws/credentials ]
 then
@@ -34,17 +35,10 @@ read COMPUTENUM
 UUID=$(cat /opt/clusterware/etc/config.yml | grep uuid | awk '{print $2}' | tr -d "'")
 TOKEN=$(cat /opt/clusterware/etc/config.yml | grep token | awk '{print $2}' | tr -d "'")
 CLUSTERNAME=`cat /opt/clusterware/etc/config.yml | grep name | awk 'NR==1{print $2}'`
-#Define CFN template
-curl -sS https://gist.githubusercontent.com/vlj91/bf35c7cae745a49a3066/raw/dcc3dc6bcfdc4325f82e5315ccb91959bdd18c0f/nodes.json > /tmp/$CLUSTERNAME-nodes.json
-sed -e "s,%AMIID%,$AMIID,g" \
-    -e "s,%INSTANCETYPE%,$INSTANCETYPE,g" \
-    -e "s,%SECGID%,$SECGID,g" \
-    -e "s,%UUID%,$UUID,g" \
-    -e "s,%TOKEN%,$TOKEN,g" \
-    -e "s,%CLUSTERNAME%,$CLUSTERNAME,g" \
-    -e "s,%MASTERNODE%,$CLUSTERMASTER,g" \
-    -e "s,%COMPUTENUM%,$COMPUTENUM,g" \
-    -e "s,%VPCID%,$VPCID,g" \
-    -e "s,%SUBNETID%,$SUBNETID,g" \
-    -i /tmp/$CLUSTERNAME-nodes.json
-aws cloudformation create-stack --stack-name $CLUSTERNAME-compute --template-body file:///tmp/$CLUSTERNAME-nodes.json
+#With AWS credentials - get ASG info
+STACKID=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names | grep AutoScalingGroupName | grep $CLUSTERNAME-ComputeGroup | awk '{print $4}')
+CURRENTCAP=$(aws autoscaling describe-auto-scaling-groups --auto-scaling-group-names $STACKID | grep DesiredCapacity | awk '{print $4}')
+DESIREDCAP=$(($CURRENTCAP+$COMPUTENUM))
+#Add desired number of instances to existing group
+aws autoscaling update-auto-scaling-group --auto-scaling-group-name $STACKID --max-size $DESIREDCAP
+aws autoscaling set-desired-capacity --auto-scaling-group-name $STACKID --desired-capacity $DESIREDCAP
