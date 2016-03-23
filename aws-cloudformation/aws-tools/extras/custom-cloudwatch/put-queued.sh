407cbb946ef6114ef4bd3a07997b73811de13a78
Vaughan Jones <vaughan.jones@alces-software.com>
2016-03-23 12:33:25 +0000

    - Custom CloudWatch monitoring and scaling: https://trello.com/c/X5LYSxT6 - Push queued jobs to custom CloudWatch metric - New template to reflect above, with IAM role for master node

---
+++ aws-cloudformation/aws-tools/extras/custom-cloudwatch/put-queued.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -l
+# Compute group name should be fed in through cloud-init or other means
+computegroup="alces1-ComputeGroup-13L5PPGC0IG6V"
+# AWS requires timestamp in the following format
+timestamp=$(date +'%Y-%m-%d'T'%T'.000Z)
+queued=$(qstat -u \* | awk 'NR > 2{print $5}' | wc -l)
+/opt/clusterware/opt/aws/bin/aws cloudwatch put-metric-data --metric-name Queued --namespace "ALCES-SGE" --dimensions "AutoScalingGroupName=${computegroup}" --value $queued --timestamp $timestamp
