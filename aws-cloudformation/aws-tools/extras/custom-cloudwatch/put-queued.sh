31b87b844bb57127062861a3f29c7783c0d5a532
Vaughan Jones <vaughan.jones@alces-software.com>
2016-03-23 12:50:33 +0000

    Add region to `put-metric-data`

--- aws-cloudformation/aws-tools/extras/custom-cloudwatch/put-queued.sh
+++ aws-cloudformation/aws-tools/extras/custom-cloudwatch/put-queued.sh
@@ -4,4 +4,4 @@ computegroup="alces1-ComputeGroup-13L5PPGC0IG6V"
 # AWS requires timestamp in the following format
 timestamp=$(date +'%Y-%m-%d'T'%T'.000Z)
 queued=$(qstat -u \* | awk 'NR > 2{print $5}' | wc -l)
-/opt/clusterware/opt/aws/bin/aws cloudwatch put-metric-data --metric-name Queued --namespace "ALCES-SGE" --dimensions "AutoScalingGroupName=${computegroup}" --value $queued --timestamp $timestamp
+/opt/clusterware/opt/aws/bin/aws cloudwatch put-metric-data --region "eu-west-1" --metric-name Queued --namespace "ALCES-SGE" --dimensions "AutoScalingGroupName=${computegroup}" --value $queued --timestamp $timestamp
